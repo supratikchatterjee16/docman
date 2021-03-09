5e6d5d04d5b3d8cdfd243017c4a489f5455455b4
supratikchatterjee16 <supratikdevm96@gmail.com>
2021-03-09 12:21:57 +0530

    Styles fix

--- docman/frontend/static/scripts/main.js
+++ docman/frontend/static/scripts/main.js
@@ -39,23 +39,23 @@ function show_overlay(content = '<input type = \"file\" placeholder=\"Upload a f
 	const overlay = document.getElementById('overlay');
 	const overlay_parser = new DOMParser();
 	const main_template = "<div class=\"container-flexible\">\
-		<div class=\"row\" style=\"height:30vh;\"></div>\
-		<div class=\"row\" style=\"height:40vh; text-align : center;\">\
+		<div class=\"row\" style=\"height:45vh;\"></div>\
+		<div class=\"row\" style=\"height:10vh; text-align : center;\">\
 			<div class=\"col-sm-4\">\
 			</div>\
-			<div class=\"col-sm\">\
-				<fieldset style=\"text-align:center;\">\
+			<div class=\"col-sm align-middle\">\
+				<fieldset class=\"align-middle\" style=\"text-align:center;\">\
 				</fieldset>\
 			</div>\
 			<div class=\"col-sm-4\">\
 			</div>\
 		</div>\
-		<div class=\"row\" style=\"height:30vh;\"></div>\
+		<div class=\"row\" style=\"height:45vh;\"></div>\
 	</div>";
 	const overlay_content = overlay_parser.parseFromString(main_template, 'text/html');
 	overlay_content.getElementsByTagName('fieldset')[0].innerHTML = content;
 	overlay.innerHTML = '';
-	overlay.appendChild(overlay_content.getRootNode().body);
+	overlay.appendChild(overlay_content.getRootNode().body.firstChild);
 	overlay.hidden = false;
 }
 function hide_overlay(){document.getElementById('overlay').hidden = true;}
